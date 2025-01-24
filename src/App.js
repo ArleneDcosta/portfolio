607376c4fd2adcdfc4b8c95ef9768d3352d68718
ArleneDcosta <arlenedcosta77@gmail.com>
2025-01-23 22:12:32 -0800

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import NavigationButtons from "./components/NavigationButtons/NavigationButtons";
 import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Projects from "./components/Projects/Projects";
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <NavigationButtons />
       <About />
       <Experience />
       <Projects />
---
+++ src/components/NavigationButtons/NavigationButtons.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./NavigationButtons.css";
+
+function NavigationButtons() {
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <section className="navigation-buttons">
+      <button onClick={() => scrollToSection("experience")}>Experience</button>
+      <button onClick={() => scrollToSection("education")}>Education</button>
+      <button onClick={() => scrollToSection("projects")}>Projects</button>
+      <button onClick={() => scrollToSection("contact")}>Contact Me</button>
+    </section>
+  );
+}
+
+export default NavigationButtons;
