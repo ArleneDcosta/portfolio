68ada31f4fad30d4cf0b8725931f75e3d1a1d10a
ArleneDcosta <arlenedcosta77@gmail.com>
2025-01-23 18:33:17 -0800

    Updated experience

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Experience.css";
-import experiences from "../../data/experienceData"
+import experiences from "./experienceData"; // Import the list
 
 function Experience() {
   const [expandedIndex, setExpandedIndex] = useState(null);
@@ -24,7 +24,13 @@ function Experience() {
             <h3>{experience.jobTitle}</h3>
             <h4>{experience.companyName}</h4>
             <span>{experience.dates}</span>
-            {expandedIndex === index && <p>{experience.description}</p>}
+            {expandedIndex === index && (
+              <ul className="description-list">
+                {experience.description.map((point, idx) => (
+                  <li key={idx}>{point}</li>
+                ))}
+              </ul>
+            )}
           </div>
         ))}
       </div>
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,25 +1,53 @@
 const experiences = [
     {
-      jobTitle: "Software Engineer",
-      companyName: "Tech Solutions Inc.",
-      dates: "Jan 2020 - Present",
-      description:
-        "Developed and maintained scalable web applications using React and Node.js. Optimized CI/CD pipelines, enhanced system performance, and collaborated with cross-functional teams.",
+      jobTitle: "Software Developer",
+      companyName: "Michigan Health Information Network Shared Services (MiHIN)",
+      dates: "Aug 2024 - Present",
+      description:[
+      "Worked on SDOH application backend for validation of FHIR-based healthcare data within S3 using AWS Lambda and CI/CD pipeline automation with IAM, Bitbucket Pipeline, Serverless, Chalice, and AWS Step Functions, reducing deployment time by 40%",
+      "Developed REST API endpoints using Jinja2 and Flask for data population on AWS Neptune using Gremlin via API Gateway with AWS Lambda as the listener, integrated with AWS Grafana, and validated using unittest and pytest",
+      "Deployed AWS resources, DynamoDB tables using Terraform, reducing manual configuration errors by 25% and improving infrastructure consistency across environments",
+      "Developed AWS Lambda functions to transition applications from AWS SQS to AWS Kinesis, enabling real-time data streaming event by implementing producers triggered by SFTP metadata uploads stored in S3 buckets",
+      "Applied design patterns to create scalable and maintainable code and data structures decreasing technical debt by 35% and improving code reusability and actively participated in code reviews to ensure code quality, maintainability, and adherence to best practices",
+      "Collaborated in an Agile environment with cross-functional teams to deliver high-quality end-to-end solutions in iterative sprints"
+    ]},
+    {
+      jobTitle: "Software Developer",
+      companyName: "Rebel",
+      dates: "May 2024 - July 2024",
+      description:[
+        "Developed a single page application using ReactJs and Django Framework with GCP Pub Sub for unstructured data storage and PostGreSQL database for enhancing metadata and log storage in a fast-paced startup environment",
+        "Designed data models, and developed serializers for REST APIs to ensure efficient data handling and communication",
+        "Implemented caching mechanism using image blob storage within tokens to reduce cloud API calls, achieving a 20% decrease in cloud API usage costs"
+      ]
     },
     {
-      jobTitle: "Frontend Developer",
-      companyName: "Creative Minds Co.",
-      dates: "May 2018 - Dec 2019",
-      description:
-        "Implemented responsive designs and dynamic features for e-commerce platforms using HTML, CSS, and JavaScript. Improved user experience and reduced page load times by 20%.",
+      jobTitle: "Senior Data Engineer",
+      companyName: "LTIMindtree",
+      dates: "Aug 2021 - Aug 2022",
+      description:[
+        "Engineered an algorithm for parallel processing of terabytes of data through multithreading, improving data processing efficiency by 38% thus creating an in-house ETL Data Pipelining strategy",
+        "Spearheaded automation of real-time data streaming from on-prem Kafka servers to Google Pub/Sub reducing latency",
+        "Implemented hashing algorithm and bucketing/Sharding concepts for data validation of petabytes of data, reducing validation time from 40 minutes to 2 minutes, and worked on Data Mesh Prototype",
+        "Spearheaded implementation of Docker containerization with Google Cloud Build and Google Cloud Run for 2+ applications, reducing deployment time by 40% and increasing deployment frequency from monthly to daily",
+        "Led design and optimization of a robust Data Ingestion automation Python Framework, reducing data loading time of diverse data sources into target data warehouse (Google BigQuery) by 20% , thus boosting reliability with Pytest units",
+        "Developed and tested a sophisticated xml parser for a Metadata Lineage tool with an inventory of 1,00,000 objects",
+        "Implemented Slowly Changing Dimensions (SCD) on Google BigQuery, utilizing Oracle Server for efficient metadata management and storage"
+
+      ]
     },
     {
-      jobTitle: "Intern - Software Development",
-      companyName: "Startup Hub",
-      dates: "Jan 2018 - Apr 2018",
-      description:
-        "Assisted in developing backend APIs using Python and Flask. Gained hands-on experience with database management and application deployment.",
-    },
+        jobTitle: "Data Engineer",
+        companyName: "LTIMindtree",
+        dates: "Aug 2020 - Aug 2021",
+        description:[
+        "Designed and implemented a Graph Sequencing Algorithm within the metadata lineage tool, strategically clustering over 10,000 objects for migration planning across complex business domains, reducing planning time by 40%",
+        "Optimized data population to GraphDB(Orientdb) using PySpark, achieving a reduction in data loading time from 48 hours to 10 minutes, resulting in a substantial increase in overall system efficiency",
+        "Engineered an automation engine that intelligently translates legacy on-prem Teradata SQL code to its Snowflake equivalent SQL code and reduces manual intervention providing 60% accuracy",
+        "Tuned complex queries with strategies reducing the Snowflake job execution time from 2 hours to 20 minutes",
+        "Implemented ETL processes using DBT (Data Build Tool) to transform, clean, and organize data, ensuring high data quality"
+        
+    ]}
   ];
   
   export default experiences;
