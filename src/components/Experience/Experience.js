a6c159c9b08ece0a6c0fd04f72285d26bcacbb14
ArleneDcosta <arlenedcosta77@gmail.com>
2025-02-06 18:21:58 -0800

    Experience design updates

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Experience.css";
-import experiences from "../../data/experienceData"; // Import the list
+import experiences from "../../data/experienceData"; 
 
 function Experience() {
   const [expandedIndex, setExpandedIndex] = useState(null);
@@ -10,31 +10,31 @@ function Experience() {
   };
 
   return (
-    <section id="experience" className="experience">
-      <h2>Experience</h2>
-      <div className="timeline">
-        {experiences.map((experience, index) => (
-          <div
-            key={index}
-            className={`timeline-item ${
-              expandedIndex === index ? "expanded" : ""
-            }`}
-            onClick={() => toggleExpand(index)}
-          >
-            <h3>{experience.jobTitle}</h3>
-            <h4>{experience.companyName}</h4>
-            <span>{experience.dates}</span>
-            {expandedIndex === index && (
-              <ul className="description-list">
-                {experience.description.map((point, idx) => (
-                  <li key={idx}>{point}</li>
-                ))}
-              </ul>
-            )}
-          </div>
-        ))}
-      </div>
-    </section>
+    <div className="experience-section">
+      <section id="experience" className="experience-container">
+        <h2 className="experience-title">Experience</h2>
+        <div className="timeline">
+          {experiences.map((experience, index) => (
+            <div
+              key={index}
+              className={`timeline-item ${expandedIndex === index ? "expanded" : ""}`}
+              onClick={() => toggleExpand(index)}
+            >
+              <h3>{experience.jobTitle}</h3>
+              <h4>{experience.companyName}</h4>
+              <span>{experience.dates}</span>
+              {expandedIndex === index && (
+                <ul className="description-list">
+                  {experience.description.map((point, idx) => (
+                    <li key={idx}>{point}</li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
 
