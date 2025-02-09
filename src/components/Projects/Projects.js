bb14166f140e3231347ce76db7fb56804ae03226
ArleneDcosta <arlenedcosta77@gmail.com>
2025-02-08 22:52:29 -0800

    Modifed projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,29 +1,31 @@
 import React from "react";
 import "./Projects.css";
-import projectData from "../../data/projectData"; // Importing project data
+import projectData from "../../data/projectData"; 
 
 function Projects() {
   return (
-    <section id="projects" className="projects">
-      <h2>Projects</h2>
-      <div className="project-cards-container">
-        <div className="project-cards">
-          {projectData.map((project, index) => (
-            <div className="card" key={index}>
-              <h3>{project.title}</h3>
-              <ul>
-                {project.description.map((point, i) => (
-                  <li key={i}>{point}</li>
-                ))}
-              </ul>
-              <a href={project.github} target="_blank" rel="noopener noreferrer">
-                GitHub 🔗
-              </a>
-            </div>
-          ))}
+    <div className="projects-section">
+      <div className="projects-container">
+        <h2 className="projects-title">Projects</h2>
+        <div className="project-cards-container">
+          <div className="project-cards">
+            {projectData.map((project, index) => (
+              <div className="card" key={index}>
+                <h3>{project.title}</h3>
+                <ul>
+                  {project.description.map((point, i) => (
+                    <li key={i}>{point}</li>
+                  ))}
+                </ul>
+                <a href={project.github} target="_blank" rel="noopener noreferrer">
+                  GitHub 🔗
+                </a>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
 
